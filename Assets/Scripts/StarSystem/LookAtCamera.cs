bcd0f2730b7a43529280f303d04b9c8f2adbff56
Brian <iso@thesquash.net>
2017-01-19 19:39:38 -0800

    Resolved performance issue and reintroduced system particle effects
    Remove huge inefficiency with the way LookAtCamera script for the system
    names was getting their camera reference. Sped everything up
    dramatically allowed me to reintroduce the particle effects we
    incorectly blamed.

--- Assets/Scripts/StarSystem/LookAtCamera.cs
+++ Assets/Scripts/StarSystem/LookAtCamera.cs
@@ -4,24 +4,27 @@ using Zenject;
 public class LookAtCamera : MonoBehaviour {
 
 	private Vector3 _lastCameraPos;
+	private Camera _camera;
 
 	[Inject]
-	public void Construct ( GameController _gameController ) {
+	public void Construct ( GameController _gameController, 
+					[Inject(Id = "MainCamera")] Camera camera ) 
+	{
 		// Dedicated server CPU usage optimization
 		// TODO: Seems like a bad dependency
 		if ( _gameController.IsHeadless() )
 			Destroy( this );
+		
+		_camera = camera;		
 	}
 	
 	void Update () {
-		if ( !Camera.main ) return;
-
 		// Don't do anything if the camera hasn't moved
-		if ( Camera.main.transform.position == _lastCameraPos )
+		if ( _camera.transform.position == _lastCameraPos )
 			return;
-		else _lastCameraPos = Camera.main.transform.position;
+		else _lastCameraPos =_camera.transform.position;
 
-		transform.LookAt(Camera.main.transform);
+		transform.LookAt(_camera.transform);
 		transform.Rotate( 0, 180, 0);
 	}
 }
