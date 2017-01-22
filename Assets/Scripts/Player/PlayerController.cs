f47b17122693749e99f96da2af7df76fcf858332
Brian <iso@thesquash.net>
2017-01-22 11:45:11 -0800

    Controlled disposal of PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -79,6 +79,7 @@ public class PlayerController : NetworkBehaviour, IDisposable {
 		_playerName = name;
 	}
 
+	public void Dispose()
 	{
 		_systemFactionChangedSignal -= TransferSystemFaction;
 		_signalDispatcher.DispatchPlayerDeparted(this);
